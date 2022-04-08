73354db0aabde8388be3c0e2388a555a071969d7
Maxim <vedernikovmaxim@rambler.ru>
2022-04-08 20:55:40 +0300

    Решена 66 задача

---
+++ Задача_66_сумма_элементов_в_диапазоне/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая
+найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNumbersMtoN(int numberM, int numberN)
+{
+    int sum = numberM;
+    if (numberM < numberN)
+    {
+        sum += SumNumbersMtoN(numberM + 1, numberN);
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число М и число N: ");
+int numberUserM = Convert.ToInt32(Console.ReadLine());
+int numberUserN = Convert.ToInt32(Console.ReadLine());
+
+int sumUser = SumNumbersMtoN(numberUserM,numberUserN);
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {numberUserM} до {numberUserN} равна: {sumUser}");
